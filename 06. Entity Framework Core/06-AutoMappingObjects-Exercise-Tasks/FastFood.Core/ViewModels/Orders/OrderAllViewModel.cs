f2a0f4e31213b0d2f69c6c30002e79a03776a227
Ivan Angelov <kontakta39@mail.bg>
2023-11-12 22:09:47 +0200

    Added better solution to the last task

---
+++ 06. Entity Framework Core/06-AutoMappingObjects-Exercise-Tasks/FastFood.Core/ViewModels/Orders/OrderAllViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FastFood.Core.ViewModels.Orders;
+
+public class OrderAllViewModel
+{
+    public int OrderId { get; set; }
+
+    public string Customer { get; set; }
+
+    public string Employee { get; set; }
+
+    public string Item { get; set; }
+
+    //This property is unnecessary, because we use DateTime.Now option in Orders View -> All
+    //public string DateTime { get; set; }
+}
