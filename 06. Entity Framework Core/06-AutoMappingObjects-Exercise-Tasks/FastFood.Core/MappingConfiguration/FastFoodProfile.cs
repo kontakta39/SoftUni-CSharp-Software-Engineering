6ebf92d439b6160fedad6ed3f36c695e96c78123
Ivan Angelov <kontakta39@mail.bg>
2023-11-12 21:07:46 +0200

    Added New Solution Task

---
+++ 06. Entity Framework Core/06-AutoMappingObjects-Exercise-Tasks/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,63 @@
+﻿namespace FastFood.Core.MappingConfiguration;
+
+using AutoMapper;
+using FastFood.Core.ViewModels.Categories;
+using FastFood.Core.ViewModels.Employees;
+using FastFood.Core.ViewModels.Items;
+using FastFood.Core.ViewModels.Orders;
+using FastFood.Models;
+using System.Security.Cryptography.Xml;
+using ViewModels.Positions;
+
+public class FastFoodProfile : Profile
+{
+    public FastFoodProfile()
+    {
+        //Positions
+        CreateMap<CreatePositionInputModel, Position>()
+            .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+        CreateMap<Position, PositionsAllViewModel>()
+            .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+        //Employees
+        CreateMap<Position, RegisterEmployeeViewModel>()
+            .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+        CreateMap<RegisterEmployeeInputModel, Employee>();
+
+        CreateMap<Employee, EmployeesAllViewModel>()
+            .ForMember(x => x.Position, y => y.MapFrom(z => z.Position.Name));
+
+        //Categories
+        CreateMap<CreateCategoryInputModel, Category>()
+            .ForMember(x => x.Name, y => y.MapFrom(z => z.CategoryName));
+        CreateMap<Category, CategoryAllViewModel>();
+
+        //Items
+        CreateMap<Category, CreateItemViewModel>()
+            .ForMember(x => x.CategoryId, y => y.MapFrom(z => z.Id));
+
+        CreateMap<CreateItemInputModel, Item>();
+
+        CreateMap<Item, ItemsAllViewModels>()
+            .ForMember(x => x.Category, y => y.MapFrom(z => z.Category.Name));
+
+        //Orders
+        CreateMap<Item, CreateOrderViewModel>()
+            .ForMember(x => x.Items, y => y.MapFrom(z => z.OrderItems));
+
+        CreateMap<CreateOrderInputModel, Order>();
+
+        CreateMap<Order, OrderAllViewModel>()
+            .ForMember(x => x.Item, y => y.MapFrom(z => z.OrderItems.Select(c => c.Item.Id)));
+
+        CreateMap<Employee, CreateOrderViewModel>()
+            .ForMember(x => x.Employees, y => y.MapFrom(z => z.Id));
+
+        CreateMap<CreateOrderInputModel, Order>();
+
+        CreateMap<Order, OrderAllViewModel>()
+           .ForMember(x => x.Employee, y => y.MapFrom(z => z.Employee.Id));
+    }
+}
---
+++ 06. Entity Framework Core/06-AutoMappingObjects-Exercise-Tasks/FastFood.Core/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FastFood.Core.ViewModels;
+
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
