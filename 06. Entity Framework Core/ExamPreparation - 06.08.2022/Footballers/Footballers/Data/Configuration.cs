fdc96b7610b713f2de1da31c35cefbcf54c724bb
Ivan Angelov <kontakta39@mail.bg>
2023-12-11 12:10:17 +0200

    Update

---
+++ 06. Entity Framework Core/ExamPreparation - 06.08.2022/Footballers/Footballers/Data/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace Footballers.Data;
+
+public static class Configuration
+{
+    public static string ConnectionString = @"Server=ACER\SQLEXPRESS;Database=Footballers;Trusted_Connection=True";
+}
